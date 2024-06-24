6ff9dee25b03c585de704e82797312730567710f
Leo <36964752+SilPan@users.noreply.github.com>
2024-06-24 14:54:33 +0200

    use long instead of int and print several array elements

--- return_local.c
+++ return_local.c
@@ -1,24 +1,28 @@
 #include <stdio.h>
 
 /**
- * Function that creates a 128 int arry and fills it with the 128 values between begin and end.
+ * Function that creates a 128 element long int arry and fills it with the 128 values between begin and end.
  */
-int** foo(int begin, int end);
+long** foo(int begin, int end);
 
 int main(void){
-    int** arr;
+    long** arr;
     arr = foo(256, 384);
-    printf("num: %i\n", *arr);
+    printf("num: %i\n", *(arr + 0));
+    printf("num: %i\n", *(arr + 1));
+    printf("num: %i\n", *(arr + 2));
     foo(128, 256);
-    printf("num: %i\n", *arr);
+    printf("num: %i\n", *(arr + 0));
+    printf("num: %i\n", *(arr + 1));
+    printf("num: %i\n", *(arr + 2));
     return 0;
 }
 
-int** foo(int begin, int end){
-    int arr[128];
+long** foo(int begin, int end){
+    long bar[128];
     for(int i = 0; i < end-begin; i++){
-        arr[i] = begin+i;
+        bar[i] = begin+i;
     }
-    int** faz = (int**) &arr;
+    long** faz = (long**) &bar;
     return faz;
 }
