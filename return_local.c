5d302a55b85b03824dd07e4b906a726af5c1e6cf
Leo <36964752+SilPan@users.noreply.github.com>
2024-06-24 15:14:44 +0200

    simplify array access

--- return_local.c
+++ return_local.c
@@ -3,26 +3,26 @@
 /**
  * Function that creates a 128 element long int arry and fills it with the 128 values between begin and end.
  */
-long** foo(int begin, int end);
+long* foo(int begin, int end);
 
 int main(void){
-    long** arr;
+    long* arr;
     arr = foo(256, 384);
-    printf("num: %i\n", *(arr + 0));
-    printf("num: %i\n", *(arr + 1));
-    printf("num: %i\n", *(arr + 2));
+    printf("num: %li\n", arr[0]);
+    printf("num: %li\n", arr[1]);
+    printf("num: %li\n", arr[2]);
     foo(128, 256);
-    printf("num: %i\n", *(arr + 0));
-    printf("num: %i\n", *(arr + 1));
-    printf("num: %i\n", *(arr + 2));
+    printf("num: %li\n", arr[0]);
+    printf("num: %li\n", arr[1]);
+    printf("num: %li\n", arr[2]);
     return 0;
 }
 
-long** foo(int begin, int end){
+long* foo(int begin, int end){
     long bar[128];
     for(int i = 0; i < end-begin; i++){
         bar[i] = begin+i;
     }
-    long** faz = (long**) &bar;
+    long* faz = (long*) &bar;
     return faz;
 }
